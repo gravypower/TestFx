297e30da43ff0d76f523afc5743e29190b726429
Matthias Koch <ithrowexceptions@gmail.com>
2015-08-16 02:05:07 +0200

    Changed Package.ps1 to remove id from nupkg's.

--- shared/build/Package.ps1
+++ shared/build/Package.ps1
@@ -15,6 +15,16 @@ Import-Module $PSScriptRoot\_Import.ps1
 
 Write-Step "Package"
 
+$TempOutputDir = Join-Path $OutputDir "Temp"
+mkdir -Force $TempOutputDir 
+
 $NuSpecFiles | %{
-  Exec { & $NuGet pack $_ -Version $SemVer -Properties Configuration=$Configuration -BasePath $SourceDir -OutputDirectory $OutputDir -Symbols -NoPackageAnalysis }
+  Exec { & $NuGet pack $_ -Version $SemVer -Properties Configuration=$Configuration -BasePath $SourceDir -OutputDirectory $TempOutputDir -Symbols -NoPackageAnalysis }
+
+  $PackageIdWithoutVersion = ([System.IO.Path]::GetFileNameWithoutExtension($_))
+  $PackageIdWithVersion = gci $TempOutputDir -Filter "*.nupkg" | select -First 1 -ExpandProperty BaseName
+
+  gci $TempOutputDir | %{
+    mv $_.FullName (Join-Path $OutputDir ("$($_.BaseName -replace $PackageIdWithVersion,$PackageIdWithoutVersion)$($_.Extension)"))
+  }
 }
